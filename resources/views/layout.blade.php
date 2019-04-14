f0ace91d42e0ffcb75a67249bff0a3ef3bd3f919
Curtis B <curtis@expertmedia.design>
2019-04-14 13:39:45 -0400

    changes

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <title>@yield('title', 'Safe-T-Flate Management App')</title>
+
+</head>
+
+<body>
+  <h3>Nav Menu</h3>
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/contact">Menu Item2</a></li>
+  </ul>
+
+
+  @yield('content')
+
+
+</body>
+
+</html>
