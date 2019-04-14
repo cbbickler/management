4e114a356c130082087cf71779bb289f9a581428
Curtis B <curtis@expertmedia.design>
2019-04-14 14:46:47 -0400

    starting product codes

---
+++ database/migrations/2019_04_14_183643_create_product_codes.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductCodes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productcodes', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('productcode');
+          $table->string('distributor');
+          $table->string('dealer');
+          $table->string('benefitspackage');
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productcodes');
+    }
+}
